82230d01a8fb8191666fb880814adf40bdac00ff
kungfu100 <thanhluan147@gmail.com>
2022-01-28 08:21:50 +0700

    update 8:21am 28/02

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -49,75 +49,89 @@ class App extends Component {
         this.state = {
             search: DEFAUTL_QUERY,
             searchKey:"",
-            data: null,
             dataCache:null,
-            dataDaily: [],
-            list:[],
             isLoading: false,
             error:"",
-
-            other:null,
         };
 
         this.needToSearch = this.needToSearch.bind(this);
-        this.updateData = this.updateData.bind(this);
-        this.getApi = this.getApi.bind(this);
+        
+        this.updateWeatherCurrent = this.updateWeatherCurrent.bind(this);
+        this.updateWeatherForecast = this.updateWeatherForecast.bind(this);
+        
+        this.getApiCurrent = this.getApiCurrent.bind(this);
+        this.getApiForeCast = this.getApiForeCast.bind(this);
+
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.onDismiss = this.onDismiss.bind(this);
-    }
-
-    updateData(data) {
-
-        this.setState(prevState => {
-            const {searchKey,} = this.state;
-            const {dataCache, list} = prevState;
-
-            return {
-               
-            }
-        });
     }
 
     needToSearch(search) {
         return !this.state.dataCache[search];
     }
 
-    getApi(search) {
+    updateWeatherCurrent(dataCurrent) {
+        this.setState(prevState => {
+            const {searchKey, dataCache,} = prevState;
+
+            const oldDataCache = dataCache ? dataCache : null 
+
+            return {
+               dataCache: {
+                   ...oldDataCache,
+                   [searchKey]: [dataCurrent,]
+               }, 
+               isLoading: false,
+            }
+        });
+    }
+
+    updateWeatherForecast(dataForecast) {
+        this.setState(prevState => {
+            const {dataCache, searchKey} = prevState;
+
+            const oldDataCacheKey = dataCache && dataCache[searchKey] 
+                ? dataCache[searchKey][0]
+                : {}
+
+            const updateDataCacheKey = [
+                oldDataCacheKey,
+                dataForecast
+            ]
+
+            return {
+                dataCache: {
+                    ...dataCache,
+                    [searchKey]: updateDataCacheKey,
+                }
+            }   
+        });
+    }
+
+    getApiCurrent(search) {
         this.setState({isLoading: true,})
 
+        //------dataCurrent---------
         axios.get(`${PATH_BASE}${PATH_WEATHER}?${PARAM_SEARCH}${search}&${PARAM_UNIT}${default_unit}&${PARAM_LANG}${default_lang}&${PARAM_KEY}${APIkey}`)
-         .then(res => this.updateData(res.data))
+         .then(res => this.updateWeatherCurrent(res.data))
          .then(() => {
-            const {data} = this.state;
-            const {coord} = data;
+            const {dataCache, searchKey} = this.state;
+            const {coord} = dataCache[searchKey][0];
 
-             axios.get(`${PATH_BASE}${PATH_ONECALL}?${PARAM_LAT}${coord.lat}&${PARAM_LON}${coord.lon}&${PARAM_EXCLUDE}${DEFAULT_EXCLUDE}&${PARAM_UNIT}${default_unit}&${PARAM_LANG}${default_lang}&${PARAM_KEY}${APIkey}`)
-              .then(res => this.setState(preveState => {
-                    const {dataDaily} = preveState;
-                    const {data} = res;
-                    const {daily} = data
-
-                    return {
-                        dataDaily: [
-                            ...dataDaily,
-                            daily,
-                        ],
-                        
-                        other: {
-                            ...preveState.other,
-                            [this.state.searchKey]: [
-                                this.state.data,
-                                data,
-                            ]
-                        }
-                    }
-              }))
+            this.getApiForeCast(coord);
          })
          .catch(error => this.setState({
             error: error.message,
             isLoading: false,
          }))
+    }
+
+    getApiForeCast(coord) {
+        const {lat, lon} = coord;
+
+        axios.get(`${PATH_BASE}${PATH_ONECALL}?${PARAM_LAT}${lat}&${PARAM_LON}${lon}&${PARAM_EXCLUDE}${DEFAULT_EXCLUDE}&${PARAM_UNIT}${default_unit}&${PARAM_LANG}${default_lang}&${PARAM_KEY}${APIkey}`)
+         .then(res => this.updateWeatherForecast(res.data)) 
     }
 
     onChange(event) {
@@ -133,63 +147,96 @@ class App extends Component {
         });
 
         if(this.needToSearch(search)) {
-          this.getApi(search);
+          this.getApiCurrent(search);
         }
 
         event.preventDefault();
     }
 
     onDismiss(id) {
-        const {list} = this.state;
+        const {dataCache, searchKey} = this.state;
 
         const isId = item => item.id !== id
-        const updateList = list.filter(isId);
+        const updateList = dataCache[searchKey][0].filter(isId);
 
-        const keyDataCache = Object.keys(this.state.dataCache);
-        let updateDataCache = {...this.state.dataCache};
+        const keyDataCache = Object.keys(dataCache);
+        let updateDataCache = {...dataCache};
     
         keyDataCache.forEach(key => {
-            if(updateDataCache[key].id === id) {
+            if(updateDataCache[key][0].id === id) {
                 delete updateDataCache[key];
             }
         })
 
         this.setState({
-            list:updateList, 
-            dataCache: updateDataCache
+            dataCache: updateDataCache,
         });
 
         //nếu id truyền vào giống trong searchKey 
         //của dataCache thì xóa nó đi
-
-        const keyOther = Object.keys(this.state.other);
-        let updateOther = {...this.state.other};
-    
-        keyOther.forEach(key => {
-            if(updateOther[key][0].id === id) {
-                delete updateOther[key];
-            }
-        })
-
-        this.setState({other: updateOther});
     }
 
     componentDidMount() {
         const {search,} = this.state;
 
-        this.setState({searchKey:search});
+        this.setState({searchKey:search},);
 
-        this.getApi(search,);
+        this.getApiCurrent(search);
     }
 
     render() {
-        const {search, list, dataDaily, isLoading, error} = this.state;
+        const {search, dataCache, isLoading, error} = this.state;
         console.log(this.state);
 
-        const _list = list || []; 
- 
+        const currentObj = null;
+        const foreCastObj = null;
+
+        const list = dataCache && dataCache[search] && dataCache[search][1]
+            ? Object.values(dataCache)
+            : []
+        
+        list.forEach((item, index) => {
+            if(index === 1) {
+                currentObj = 
+            }
+        })
+
         return(
             <div className="app">
+                <Search
+                    value={search}
+                    onChange={this.onChange}
+                    onSubmit={this.onSubmit}
+                />
+                
+                <main>
+                    <header>
+                        <h1>Daily Weather</h1>
+                    </header>
+
+                  {/*
+                        list(value => {
+
+
+                            return(
+                             <div className="wrap-padding-bottom">
+                                 <div className="table">
+                                    <div className="table-top">
+                                        {}
+                                    </div>
+
+                                    <div className="table-bottom">
+
+                                    </div>
+                                 </div>
+                             </div>   
+                        )})*/
+                    }
+                </main>
+            </div>
+
+
+            /*<div className="app">
                 <Search
                     value={search}
                     onChange={this.onChange}
@@ -211,7 +258,7 @@ class App extends Component {
                     />
                     
                 </main>
-            </div>
+            </div>*/
     )}
 }
 
